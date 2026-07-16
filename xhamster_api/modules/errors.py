5f8b30bd64cc82332e17d353aa7c065d871391e6
Rakib9851 <rakibffbd66@gmail.com>
2026-07-17 01:33:33 +0600

    Add custom exception classes for error handling

---
+++ xhamster_api/modules/errors.py
@@ -0,0 +1,6 @@
+class NotFound(Exception): pass
+class NetworkError(Exception): pass
+class BotDetection(Exception): pass
+class ProxyError(Exception): pass
+class UnknownNetworkError(Exception): pass
+class DownloadFailed(Exception): pass
